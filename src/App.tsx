6a4d8b99d9bde6bc4b2606d6a5d2b465a56c34ff
jigsaw <m@jgs.me>
2023-03-18 22:56:50 +0900

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 type Page = {
   title: string;
   id: string;
 };
 
+type Project = {
+  pages: Page[];
+};
+
 function App() {
-  const [pages, setPages] = useState<Page[]>([]);
+  const [original, setOriginal] = useState<Project | null>(null);
+  const [originalPagesMap, setOriginalPagesMap] = useState(
+    new Map<string, Page>()
+  );
+  const [selected, setSelected] = useState(new Map<string, Page>());
   const onChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
     async (event) => {
       if (event.currentTarget.files) {
@@ -14,30 +22,70 @@ function App() {
           const file = event.currentTarget.files[0];
           const text = await file.text();
           const json = JSON.parse(text);
-          setPages(json.pages);
+          setOriginal(json);
+          const map = new Map<string, Page>();
+          json.pages.forEach((page: Page) => {
+            map.set(page.id, page);
+          });
+          setOriginalPagesMap(map);
         }
       }
     },
     []
   );
+  const [query, setQuery] = useState("");
+  const textOnChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
+    (event) => {
+      setQuery(event.currentTarget.value);
+    },
+    []
+  );
+  const candidate = useMemo(() => {
+    const next = new Map<string, Page>();
+    originalPagesMap.forEach((page, key) => {
+      if (page.title.indexOf(query) > -1) {
+        next.set(key, page);
+      }
+    });
+    return next;
+  }, [originalPagesMap, query]);
   return (
-    <div>
+    <div className="grid grid-cols-3">
       <div>
-        <label htmlFor="original" className="mr-2">
-          Original:
+        <div>
+          <label htmlFor="original" className="block text-4xl text-center">
+            Original
+          </label>
+          <div className="text-center">
+            <input
+              type="file"
+              onChange={onChange}
+              id="original"
+              accept=".json"
+            ></input>
+          </div>
+        </div>
+        <div>
+          {original &&
+            original.pages
+              .slice(0, 100)
+              .map((page) => <div key={page.id}>{page.title}</div>)}
+        </div>
+      </div>
+      <div>
+        <h1 className="text-4xl text-center">Selected</h1>
+        <label htmlFor="query" className="mr-2">
+          Query:
         </label>
         <input
-          type="file"
-          onChange={onChange}
-          id="original"
-          accept=".json"
+          type="text"
+          value={query}
+          onChange={textOnChange}
+          className="border"
+          id="query"
         ></input>
       </div>
-      <div>
-        {pages.map((page) => (
-          <div key={page.id}>{page.title}</div>
-        ))}
-      </div>
+      <div>dest</div>
     </div>
   );
 }
