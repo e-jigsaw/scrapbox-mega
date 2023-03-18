6b61917fa435b18a893c215bce5357164ae013cd
jigsaw <m@jgs.me>
2023-03-18 19:19:58 +0900

    show json

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,45 @@
+import { useCallback, useState } from "react";
+
+type Page = {
+  title: string;
+  id: string;
+};
+
 function App() {
-  return <h1 className="text-6xl">It works!</h1>;
+  const [pages, setPages] = useState<Page[]>([]);
+  const onChange = useCallback<React.ChangeEventHandler<HTMLInputElement>>(
+    async (event) => {
+      if (event.currentTarget.files) {
+        if (event.currentTarget.files?.length > 0) {
+          const file = event.currentTarget.files[0];
+          const text = await file.text();
+          const json = JSON.parse(text);
+          setPages(json.pages);
+        }
+      }
+    },
+    []
+  );
+  return (
+    <div>
+      <div>
+        <label htmlFor="original" className="mr-2">
+          Original:
+        </label>
+        <input
+          type="file"
+          onChange={onChange}
+          id="original"
+          accept=".json"
+        ></input>
+      </div>
+      <div>
+        {pages.map((page) => (
+          <div key={page.id}>{page.title}</div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
